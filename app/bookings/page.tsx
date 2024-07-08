4a61cd021f56c8a644ff072fcb52d13ac64516d6
Himanshu Sharma <sharmahimanshu050104@gmail.com>
2024-07-08 23:10:26 +0530

    Initialized Project and Set up the required directories

---
+++ app/bookings/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Bookingspage() {
+  return <h1 className="text-3xl">Bookings Page</h1>;
+}
+
+export default Bookingspage;
---
+++ app/checkout/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Checkoutpage() {
+  return <h1 className="text-3xl">Checkout</h1>;
+}
+
+export default Checkoutpage;
